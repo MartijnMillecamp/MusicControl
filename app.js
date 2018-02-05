1f5d5ee61cc2988cb4231f2edd82e3405e027902
Unknown <martijn.millecamp@hotmail.com>
2018-02-05 10:17:11 +0100

    working online

--- app.js
+++ app.js
@@ -17,34 +17,35 @@ var app = express();
 
 
 // var mongoDB = "mongodb://127.0.0.1/spotify_test";
+var config = JSON.parse(process.env.APP_CONFIG);
+var mongoDB = "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + config.mongo.hostString;
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 
 
-// mongoose.connect(mongoDB, function (err) {
-//     if (err) {
-//         console.log("connection error", err);
-//
-//     } else {
-//         console.log('connection successful!');
-//     }
-// });
-var config = JSON.parse(process.env.APP_CONFIG);
-var MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect(
-	"mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" +
-	config.mongo.hostString,
-	function(err, db) {
-		if (err) {
+mongoose.connect(mongoDB, function (err) {
+    if (err) {
         console.log("connection error", err);
 
     } else {
         console.log('connection successful!');
     }
-	}
-);
+});
+
+// var MongoClient = require('mongodb').MongoClient;
+// MongoClient.connect(
+// 	"mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" +
+// 	config.mongo.hostString,
+// 	function(err, db) {
+// 		if (err) {
+//         console.log("connection error", err);
+//
+//     } else {
+//         console.log('connection successful to ' + db );
+//     }
+// 	}
+// );
 
 
 app.set('trust proxy', 1); // trust first proxy
@@ -54,9 +55,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/public", express.static(path.join(__dirname, 'public')));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
