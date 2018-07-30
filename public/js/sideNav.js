80bb73482febdc1375cdd86ccfffe7317e06f207
Unknown <martijn.millecamp@hotmail.com>
2018-07-30 14:17:17 +0200

    logging sidenav

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -1,12 +1,13 @@
 function openNav() {
 	$('#column4').css('width', '400px');
 	$('#mySidenav').css('width', '400px');
+	addInteraction('sideNav', 'click', 'open')
 }
 
 function closeNav() {
 	$('#mySidenav').css('width', '0');
 	$('#column4').css('width', '100px');
-
+	addInteraction('sideNav', 'click', 'close')
 }
 
 
