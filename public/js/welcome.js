f7ce72486deda072dd9fd68e36d687a2b9362d05
Unknown <martijn.millecamp@hotmail.com>
2018-08-02 11:40:35 +0200

    welcome page

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -3,5 +3,8 @@ $( document ).ready(function() {
 	$('#button_welcome').click(function (event) {
 		window.location.href = base + '/auth/spotify';
 	});
+	$('#button_disagree').click(function (event) {
+		window.location.href = base + '/thanks';
+	});
 });
 
