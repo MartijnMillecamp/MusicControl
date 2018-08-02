d720acf5c6d95ac314d5c7619859612d4bf21936
Unknown <martijn.millecamp@hotmail.com>
2018-08-02 15:38:12 +0200

    auto fill in questionnaire

--- public/js/questionnaires.js
+++ public/js/questionnaires.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
-
-
+	// https://docs.google.com/forms/d/e/1FAIpQLSd-bO5IL4Kt6TdFvCrJAgg26t7xAJHW48WI8oDH-lnyYR6Snw/viewform?usp=pp_url&entry.452024117=test
+	console.log(userID)
+	var src = "https://docs.google.com/forms/d/e/1FAIpQLSd-bO5IL4Kt6TdFvCrJAgg26t7xAJHW48WI8oDH-lnyYR6Snw/viewform?usp=pp_url&entry.452024117=";
+	src += userID ;
+	var backup = "https://goo.gl/forms/8uCLRa92uRMHUiCi1?embedded=true&amp;hl=en"
+	document.getElementById('iframePC').src = src
 
 	$('#button_questionnaires').click(function (event) {
 		var height = window.screen.availHeight;
@@ -11,9 +15,7 @@ $( document ).ready(function() {
 		var query = base + '/addUser?' + queryUser + queryAdmin;
 
 		$.getJSON( query, function( message ) {
-			console.log(message)
 			window.location.href = base + '/attributes';
 		});
-
 	});
 });
